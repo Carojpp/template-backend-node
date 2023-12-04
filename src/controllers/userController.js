a45acfcda0cc098415b880c1558e6f50db0688af
Carojpp <carolyn.pena.padilla@gmail.com>
2023-12-04 12:23:24 -0500

    CM-010 modify userController - add models

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,33 +1,9 @@
-const userDB = [
-    {
-        id: '1',
-        name: 'Ed',
-        email: 'ed@example.com',
-        phone: '3016453022',
-        country: 'CO',
-        createdAt: new Date()
-    },
-    {
-        id: '2',
-        name: 'Caro',
-        email: 'caro@example.com',
-        phone: '3147107177',
-        country: 'ES',
-        createdAt: new Date()
-    },
-    {
-        id: '3',
-        name: 'Sam',
-        email: 'sam@example.com',
-        phone: '3016445522',
-        country: 'BR',
-        createdAt: new Date()
-    },
-]
+import { getUsersModels } from "../models/userModel.js"
 
 const getUsers = () => {
-    console.log ('file: userController.js - getUsers')
-    return userDB
+    console.log('file: userController.js - getUsers')
+    const users = getUsersModels()
+    return users
 }
 export {
     getUsers
