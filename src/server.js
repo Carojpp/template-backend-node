f56f8586a539ce081d26d30a0eb4114d54462438
Edwin Antonio Arroyo Lopez <edwin.antonio.arroyo.lopez@gmail.com>
2023-12-13 12:24:49 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser'
 
 /* call routes */
@@ -6,10 +7,12 @@ import accountRoutes from './routes/accountRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
 const app = express();
+
 const PORT = 7000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use(accountRoutes)
 app.use(userRoutes)
